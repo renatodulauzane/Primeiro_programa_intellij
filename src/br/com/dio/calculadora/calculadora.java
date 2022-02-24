5e933a74750e93f876a7182b98789d8fcbac8eae
renatodulauzane <renatodulauzane@hotmail.com>
2022-02-24 08:20:11 -0300

    second commit

---
+++ src/br/com/dio/calculadora/calculadora.java
@@ -0,0 +1,35 @@
+package br.com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class calculadora {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+                int a, b;
+        System.out.println("digite o primeiro valor: ");
+        a = scan.nextInt();
+        System.out.println("digite o segundo valor: ");
+        b = scan.nextInt();
+
+        int somar = somar(a, b);
+        int subtrair = subtrair(a, b);
+        int multiplicar = multiplicar(a, b);
+        int dividir = dividir(a, b);
+
+        System.out.println("somar: " + somar);
+        System.out.println("subtrair: " + subtrair);
+        System.out.println("dividir: " + dividir);
+        System.out.println("multiplicar: " + multiplicar);
+
+
+    }
+    public static int somar(int a, int b) {
+        return a + b;
+    } public static int subtrair(int a, int b) {
+        return a - b;
+    } public static int dividir(int a, int b) {
+        return a / b;
+    } public static int multiplicar(int a, int b) {
+        return a * b;
+    }
+}
